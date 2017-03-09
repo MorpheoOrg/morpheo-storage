b17c138c50d0a9b23d6b7de6a3b9a88bd7a73ced
Étienne Lafarge <etienne.lafarge@gmail.com>
2017-03-09 11:46:20 +0100

    Parse data structures in API routes

--- api/main.go
+++ api/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+
 	"gopkg.in/kataras/iris.v6"
-	"gopkg.in/kataras/iris.v6/adaptors/httprouter" // <--- or adaptors/gorillamux
+	"gopkg.in/kataras/iris.v6/adaptors/httprouter" // <--- TODO or adaptors/gorillamux
+	"gopkg.in/kataras/iris.v6/middleware/logger"
+
+	"github.com/DeepSee/dc-compute"
 )
 
 func main() {
+	// Iris setup
 	app := iris.New()
+	app.Adapt(iris.DevLogger())
 	app.Adapt(httprouter.New())
+
+	// Logger middleware configuration
+	customLogger := logger.New(logger.Config{
+		Status: true,
+		IP:     true,
+		Method: true,
+		Path:   true,
+	})
+	app.Use(customLogger)
 
 	// Handlers configuration
 	app.Get("/", index)
-	app.Post("/train", postTrainuplet)
+	app.Post("/learn", postLearnuplet)
 	app.Post("/pred", postPreduplet)
-	app.Post("/train_task", postTrainTask)
+	app.Post("/learn_task", postLearnTask)
 	app.Post("/test_task", postTestTask)
 
 	// Main server loop
@@ -21,36 +39,103 @@ func main() {
 }
 
 func index(c *iris.Context) {
-	c.JSON(iris.StatusOK, []string{"train", "predict"})
+	c.JSON(iris.StatusOK, []string{"/learn", "/pred", "/learn_task", "/test_task"})
 }
 
-func postTrainuplet(c *iris.Context) {
-	// TODO: parse that
-	// TODO: split learnuplet into LearnTasks and TestTask
+func postLearnuplet(c *iris.Context) {
+	var learnUplet dccompute.LearnUplet
+
+	// Unserializing the request body
+	if err := json.NewDecoder(c.Request.Body).Decode(&learnUplet); err != nil {
+		msg := fmt.Sprintf("Error decoding body to JSON: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
+
+	// Let's check for required arguments presence and validity
+	if err := learnUplet.Check(); err != nil {
+		msg := fmt.Sprintf("Invalid learn-uplet: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
+
+	firstTask := learnUplet.SplitTrain()
+
+	log.Println(firstTask.Next.Data.String())
 	// TODO: notify the orchestrator we're starting this learning process
 	// TODO: ask storage for the cluster to send the first task to (shouldn't it rather be set by the
 	// orchestrator ?)
-	// TODO: send that damn task
-	c.JSON(iris.StatusAccepted, map[string]string{"message": "Train-uplet Ingested"})
+	// TODO: send that damn task to the appropriate compute cluster
+	c.JSON(iris.StatusAccepted, map[string]string{"message": "Learn-uplet Ingested"})
 }
 
 func postPreduplet(c *iris.Context) {
-	// TODO: parse that
+	var predUplet dccompute.Preduplet
+
+	// Unserializing the request body
+	if err := json.NewDecoder(c.Request.Body).Decode(&predUplet); err != nil {
+		msg := fmt.Sprintf("Error decoding body to JSON: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
+
+	// Let's check for required arguments presence and validity
+	if err := predUplet.Check(); err != nil {
+		msg := fmt.Sprintf("Invalid pred-uplet: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
 	// TODO: notify the orchestrator we're starting this prediction process
-	// TODO: send that damn task
+	// TODO: send task to broker
 	c.JSON(iris.StatusAccepted, map[string]string{"message": "Pred-uplet ingested"})
 }
 
-func postTrainTask(c *iris.Context) {
-	// TODO: parse that
+func postLearnTask(c *iris.Context) {
+	var learnTask dccompute.LearnTask
+
+	// Unserializing the request body
+	if err := json.NewDecoder(c.Request.Body).Decode(&learnTask); err != nil {
+		msg := fmt.Sprintf("Error decoding body to JSON: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
+
+	// Let's check for required arguments presence and validity
+	if err := learnTask.Check(); err != nil {
+		msg := fmt.Sprintf("Invalid learn-task: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
 	// TODO: notify the orchestrator we're starting this prediction process
-	// TODO: send that damn task
-	c.JSON(iris.StatusAccepted, map[string]string{"message": "Train task ingested"})
+	// TODO: send task to broker
+	c.JSON(iris.StatusAccepted, map[string]string{"message": "Learn task ingested"})
 }
 
 func postTestTask(c *iris.Context) {
-	// TODO: parse that
+	var testTask dccompute.TestTask
+
+	// Unserializing the request body
+	if err := json.NewDecoder(c.Request.Body).Decode(&testTask); err != nil {
+		msg := fmt.Sprintf("Error decoding body to JSON: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
+
+	// Let's check for required arguments presence and validity
+	if err := testTask.Check(); err != nil {
+		msg := fmt.Sprintf("Invalid test-task: %s", err)
+		log.Printf("[INFO] %s", msg)
+		c.JSON(iris.StatusBadRequest, map[string]string{"message": msg})
+		return
+	}
 	// TODO: notify the orchestrator we're starting this prediction process
-	// TODO: send that damn task
+	// TODO: send task to broker
 	c.JSON(iris.StatusAccepted, map[string]string{"message": "Test task ingested"})
 }
--- data_structures.go
+++ data_structures.go
@@ -1,53 +1,148 @@
 package dccompute
 
-import "github.com/satori/go.uuid"
+import (
+	"fmt"
+
+	"github.com/satori/go.uuid"
+)
+
+type Checkable interface {
+	Check() (err error)
+}
 
 // Defines a pred-uplet
 type Preduplet struct {
+	Checkable
+
 	Id      uuid.UUID   `json:"id"`
 	Problem uuid.UUID   `json:"problem"`
-	Data    []uuid.UUID `json:"data"` // ? Why a list field here ?
 	Model   uuid.UUID   `json:"model"`
+	Data    []uuid.UUID `json:"data"` // ? Why a list field here ?
+}
+
+func (u *Preduplet) Check() (err error) {
+
+	if uuid.Equal(uuid.Nil, u.Id) {
+		return fmt.Errorf("id field is unset")
+	}
+
+	if uuid.Equal(uuid.Nil, u.Problem) {
+		return fmt.Errorf("problem field is unset")
+	}
+
+	if uuid.Equal(uuid.Nil, u.Model) {
+		return fmt.Errorf("model field is required")
+	}
+
+	if len(u.Data) == 0 {
+		return fmt.Errorf("train_data is empty or unset")
+	}
+	for _, id := range u.Data {
+		if uuid.Equal(uuid.Nil, id) {
+			return fmt.Errorf("Nil UUID in dataset")
+		}
+	}
+
+	return nil
 }
 
 // Defines a learn-uplet
 type LearnUplet struct {
+	Checkable
+
 	Id             uuid.UUID   `json:"id"`
 	Problem        uuid.UUID   `json:"problem"`
+	Model          uuid.UUID   `json:"model"`
 	LearnDataset   []uuid.UUID `json:"train_data"`
 	TestDataset    []uuid.UUID `json:"test_data"`
 	IndividualPerf []float64   `json:"individual_perf"`
 }
 
+func (u *LearnUplet) Check() (err error) {
+
+	if uuid.Equal(uuid.Nil, u.Id) {
+		return fmt.Errorf("id field is required")
+	}
+
+	if uuid.Equal(uuid.Nil, u.Problem) {
+		return fmt.Errorf("problem field is required")
+	}
+
+	if uuid.Equal(uuid.Nil, u.Model) {
+		return fmt.Errorf("model field is required")
+	}
+
+	if len(u.LearnDataset) == 0 {
+		return fmt.Errorf("train_data is empty or unset")
+	}
+	for _, id := range u.LearnDataset {
+		if uuid.Equal(uuid.Nil, id) {
+			return fmt.Errorf("Nil UUID in LearnDataset")
+		}
+	}
+	if len(u.TestDataset) == 0 {
+		return fmt.Errorf("test_data is empty or unset")
+	}
+	for _, id := range u.TestDataset {
+		if uuid.Equal(uuid.Nil, id) {
+			return fmt.Errorf("Nil UUID in LearnDataset")
+		}
+	}
+
+	return nil
+}
+
 // Splits the train-uplet into a linked list of LearnTasks
 func (u *LearnUplet) SplitTrain() (firstTask *LearnTask) {
-	nest(firstTask, u.TrainDataset)
+	u.nest(&firstTask, u.LearnDataset)
 	return
 }
 
 // A tiny little bit of tail recursion doesn't hurt once in a while
-func nest(ptr *LearnTask, slice []uuid.UUID) {
+func (u *LearnUplet) nest(ptr **LearnTask, slice []uuid.UUID) {
 	if len(slice) <= 0 {
 		return
 	}
 
-	ptr = &LearnTask{
+	*ptr = &LearnTask{
 		Data:       slice[0],
-		LearnUplet: &u,
+		LearnUplet: u,
 	}
-	nest(ptr.Next, slice[1:])
+	u.nest(&(*ptr).Next, slice[1:])
 }
 
 // Defines a tearning task on one unit of data
 type LearnTask struct {
+	Checkable
+
 	Data       uuid.UUID   `json:"data"`
 	Next       *LearnTask  `json:"next"`
 	LearnUplet *LearnUplet `json:learn-uplet`
 }
 
+func (u *LearnTask) Check() (err error) {
+
+	if uuid.Equal(uuid.Nil, u.Data) {
+		return fmt.Errorf("data field is unset")
+	}
+
+	return nil
+}
+
 // Defines a test task
 type TestTask struct {
+	Checkable
+
 	Data       uuid.UUID   `json:"data"`
 	Next       *TestTask   `json:"next"`
 	LearnUplet *LearnUplet `json:"learn-uplet"`
+}
+
+func (u *TestTask) Check() (err error) {
+
+	if uuid.Equal(uuid.Nil, u.Data) {
+		return fmt.Errorf("data field is unset")
+	}
+
+	return nil
 }
