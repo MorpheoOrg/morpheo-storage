47e23480ccee0580982429f14b8699053af950f6
Max-Pol <maxpol.lebrun_at_gmail.com>
2017-06-14 14:27:04 +0200

    refactoring: clean readme & startdev

--- startdev.sh
+++ startdev.sh
@@ -8,13 +8,19 @@ fi
 
 if [ "$BUILD" -ne "0" ]; then
     echo ""
-    echo "Compiling Go storage packages..."
+    echo "Compiling Go storage packages from:"
+    # print git branches from which binary will be built
+    echo "	- storage branch: " $(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+    echo "	- go-packages branch: " $(cd ../go-packages; git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+    # remove old binary and compile new
     rm build/target &> /dev/null
     CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o build/target .
 fi
 
-
-echo ""
-echo "Build and run docker containers..."
-# sudo chmod 777 -R data
-STORAGE_PORT=8081 STORAGE_AUTH_USER=u STORAGE_AUTH_PASSWORD=p docker-compose up --build
+if [ -e build/target ]; then
+	echo ""
+	echo "Build and run docker containers..."
+	# sudo chmod 777 -R data
+	STORAGE_PORT=8081 STORAGE_AUTH_USER=u STORAGE_AUTH_PASSWORD=p docker-compose up --build
+fi
